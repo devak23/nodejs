b527e04fb25854f8cc0d79edef5769cb165393ab
devak23 <developer.ak23@gmail.com>
2017-08-02 19:41:27 +0530

    Cleanup

--- src/read_file.js
+++
@@ -1,7 +0,0 @@
-console.log('Reading a file');
-var content = '';
-var fs = require('fs');
-fs.readFile('./sherlock.txt', 'UTF8', function(err, data) {
-  content = data;
-  console.log("Content: ", data);
-});
--- src/test/appTest.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert;
-const app = require('../app');
-
-describe('App', () => {
-    "use strict";
-   it ('App should return Hello', () => {
-       assert.equal(app(), 'hello');
-   })
-});
