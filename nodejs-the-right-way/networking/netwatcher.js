ae6bc2ab1875e4ad2d084c97711bdf7d4e565d2f
devak23 <developer.ak23@gmail.com>
2017-08-08 22:52:09 +0530

    added a simple server

---
+++ nodejs-the-right-way/networking/netwatcher.js
@@ -0,0 +1,37 @@
+//
+// 03. Writing Data to a Socket
+//
+
+"use strict";
+const
+    net = require('net'),
+    fs = require('fs'),
+    filename = process.argv[2];
+
+if (!filename) {
+    console.log("No target file was specified");
+    return;
+}
+
+try {
+    fs.accessSync(filename, fs.R_OK | fs.W_OK);
+} catch (e) {
+    console.log('The file', filename, 'does not exist');
+    return;
+}
+
+const server = net.createServer((connection) => {
+    console.log("subscriber connected...");
+    connection.write("Now watching " + filename + " for changes...\n");
+
+    let watcher = fs.watch(filename, () => {
+        connection.write("File " + filename + " changed: " + Date.now() + "\n");
+    });
+
+    connection.on('close', () => {
+        console.log("subscriber disconnected");
+        watcher.close();
+    })
+}).listen(5432, () => {
+    console.log("server started on port 5432\nListening for subscribers...");
+});
