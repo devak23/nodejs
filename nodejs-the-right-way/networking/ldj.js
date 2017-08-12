c2dfaabc3747160015887eadb21a4efddc2c8f61
devak23 <developer.ak23@gmail.com>
2017-08-12 22:42:21 +0530

    Adding comments

--- nodejs-the-right-way/networking/ldj.js
+++ nodejs-the-right-way/networking/ldj.js
@@ -5,10 +5,12 @@
 const
     events = require('events'),
     util = require('util'),
-// Define a LDJCLient class. This is the constructor
+
+    // Define a LDJCLient class. This is the constructor
     LDJClient = function (stream) {
         // the input parameter stream is the one that emits event
         events.EventEmitter.call(this); // this is roughly equivalent of calling super() in Java
+        // what we are saying is LDJClient is a child class of EventEmitter
 
         let
             self = this,
@@ -16,18 +18,21 @@ const
         stream.on('data', (data) => {
             // keep on appending the data
             buffer += data;
-            // if a newline is sent, get the index
+
+            // look for the newline, if sent, get the index
             let boundary = buffer.indexOf('\n');
+
             // if you do get a valid index (which means you received a '\n',
             // then process the message
-            while (boundary !== -1) {
+            if (boundary !== -1) {
                 // take the entire input from the buffer
                 let input = buffer.substr(0, boundary);
+
                 // empty the buffer
                 buffer = buffer.substr(boundary+1);
+
                 // emit the message
                 self.emit('message', JSON.parse(input));
-                boundary = buffer.indexOf('\n');
             }
         });
 
